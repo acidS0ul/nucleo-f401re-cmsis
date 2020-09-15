eeb1c1b18acd1fe8ae14464e338c0de2178054cc
Denis Semenov <37097033+0xSOUL@users.noreply.github.com>
2020-09-15 18:56:53 +0300

    updated main.c
    updated main. c: added tracking of the button state based on which the led will be lit

--- Core/src/main.c
+++ Core/src/main.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
+#define LED2_ON		GPIOA->ODR = 0x20
+#define LED2_OFF	GPIOA->ODR = 0
+
+void InitGPIO(void)
+{
+	// LED2
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODE5_0;
+	GPIOA->OTYPER  = 0;
+	GPIOA->OSPEEDR = 0;
+	// BUTTON1
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+}
+
 int main(void)
 {
-	while(1);
+	SystemInit();
+	InitGPIO();
+
+	while(1)
+	{
+		if(!READ_BIT(GPIOC->IDR,GPIO_IDR_ID13))
+		{
+			LED2_ON;
+		}
+		LED2_OFF;
+	}
 }
